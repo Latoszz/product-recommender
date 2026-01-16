580ee501ea1c9c233f56163f349e166d956dc66b
Latoszz <118390289+Latoszz@users.noreply.github.com>
2026-01-16 12:33:25 +0100

    First draft

---
+++ connection_manager.py
@@ -0,0 +1,107 @@
+from neo4j import GraphDatabase
+from pyvis.network import Network
+from dotenv import load_dotenv
+from os import getenv
+
+
+load_dotenv()
+
+class GraphApp:
+    def __init__(self):
+        self.uri = getenv("NEO4J_URI")
+        self.user = getenv("NEO4J_USER")
+        self.password = getenv("NEO4J_PASSWORD")
+
+        self.driver = GraphDatabase.driver(self.uri, auth=(self.user, self.password))
+        self.driver.verify_connectivity()
+
+    def close(self):
+        self.driver.close()
+
+    def run_query(self, query, parameters=None):
+        with self.driver.session() as session:
+            result = session.run(query, parameters)
+            return [record.data() for record in result]
+
+    def add_user(self, name):
+        query = "MERGE (u:User {name: $name})"
+        self.run_query(query, {"name": name})
+
+    def add_product(self, name):
+        query = "MERGE (p:Product {name: $name})"
+        self.run_query(query, {"name": name})
+
+    def follow_user(self, user1, user2):
+        query = """
+        MATCH (a:User {name: $u1}), (b:User {name: $u2})
+        MERGE (a)-[:FOLLOWS]->(b)
+        """
+        self.run_query(query, {"u1": user1, "u2": user2})
+
+    def rate_product(self, user, product, rating, label):
+        query = """
+        MATCH (u:User {name: $user}), (p:Product {name: $product})
+        MERGE (u)-[r:RATES]->(p)
+        SET r.rating = $rating, r.type = $label
+        """
+        self.run_query(
+            query, {"user": user, "product": product, "rating": rating, "label": label}
+        )
+
+    def get_users(self):
+        return [
+            x["name"] for x in self.run_query("MATCH (u:User) RETURN u.name as name")
+        ]
+
+    def get_products(self):
+        return [
+            x["name"] for x in self.run_query("MATCH (p:Product) RETURN p.name as name")
+        ]
+
+
+    def rec_by_friends(self, user_name, min_friends=1):
+        query = """
+        MATCH (u:User {name: $name})-[:FOLLOWS]->(friend)-[r:RATES]->(p:Product)
+        WHERE r.rating >= 4  // Zakładamy, że polecenie to ocena >= 4
+        WITH p, count(friend) as friend_count
+        WHERE friend_count >= $min_friends
+        RETURN p.name as Produkt, friend_count as Liczba_Polecen
+        ORDER BY friend_count DESC
+        """
+        return self.run_query(query, {"name": user_name, "min_friends": min_friends})
+
+    def rec_collaborative(self, user_name):
+        query = """
+        MATCH (u:User {name: $name})-[r1:RATES]->(p:Product)<-[r2:RATES]-(other:User)
+        WHERE abs(r1.rating - r2.rating) <= 1  // Znajdź użytkowników o podobnym guście (podobne oceny tych samych produktów)
+        WITH other, u
+        MATCH (other)-[r3:RATES]->(rec_p:Product)
+        WHERE NOT (u)-[:RATES]->(rec_p) AND r3.rating >= 4 // Znajdź co oni lubią, a czego ja nie ocenilem
+        RETURN rec_p.name as Produkt, count(*) as Waga_Rekomendacji
+        ORDER BY Waga_Rekomendacji DESC
+        """
+        return self.run_query(query, {"name": user_name})
+
+    def visualize_user_graph(self, user_name):
+        query = """
+        MATCH path = (u:User {name: $name})-[*1..2]-(node)
+        RETURN path
+        """
+        results = self.driver.session().run(query, name=user_name)
+
+        net = Network(
+            height="500px", width="100%", bgcolor="#222222", font_color="white"
+        )
+
+        for record in results:
+            for node in record["path"].nodes:
+                color = "#00ff41" if "User" in node.labels else "#ff0055"
+                label = node["name"]
+                net.add_node(node.element_id, label=label, title=label, color=color)
+
+            for rel in record["path"].relationships:
+                net.add_edge(
+                    rel.start_node.element_id, rel.end_node.element_id, title=rel.type
+                )
+
+        return net
